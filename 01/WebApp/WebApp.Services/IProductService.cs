31500d5f1e207b4ae8a7efe0e910035d7d6a3475
Liam Liang <ryou1234@outlook.com>
2019-03-08 22:41:51 +0800

    add webapp.services project

---
+++ 01/WebApp/WebApp.Services/IProductService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+
+namespace WebApp.Services
+{
+    public interface IProductService<T> where T : class
+    {
+        IEnumerable<T> SomethingSpecialMethod(Expression<Func<T, bool>> predicate);
+    }
+}
---
+++ 01/WebApp/WebApp.Services/ProductService.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+
+using LYLStudio.Service.Data.EntityFramework;
+
+using WebApp.Models;
+
+namespace WebApp.Services
+{
+    public class ProductService<T> : DataServiceBase<DataAccessResult, NorthwindEntities>, IProductService<T> where T :class
+    {
+        private NorthwindEntities _context;
+        public override NorthwindEntities Context => _context ?? (_context = new NorthwindEntities());
+
+        public override Action<string> Log
+        {
+            get => Context.Database.Log;
+            set => Context.Database.Log = value;
+        }
+
+        public IEnumerable<T> SomethingSpecialMethod(Expression<Func<T, bool>> predicate)
+        {
+            return this.FetchAll<T>().AsQueryable().Where(predicate);
+        }
+    }
+}
